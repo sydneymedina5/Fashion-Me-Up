a44a42f6ff470fdac4f5fe44531a47512efaa922
Sydney Medina <sydneymedina5@gmail.com>
2023-09-16 10:08:42 -0500

    Update .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Fashion-Me-Up",
+    version="1.0",
+    description="AI Model To Pick A Fashionable Outfit",
+    author="Sydney Medina",
+    author_email="sydneymedina5@gmail.com",
+    packages=["data", "deep_learning", "creative"],
+    # FIX THIS lmao
+)
